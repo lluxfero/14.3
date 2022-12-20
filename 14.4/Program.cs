bfda7e8bbc9430d376f7da012724394061d90edb
Анастасия Овечкина <onastja02@gmail.com>
2022-12-20 17:18:54 +0300

    new

--- 14.4/Program.cs
+++ 14.4/Program.cs
@@ -17,18 +17,20 @@ Console.WriteLine("\n");
 AutoResetEvent waiter = new(true);
 int n = 0;
 Stopwatch time = Stopwatch.StartNew();
-arr.AsParallel()
+var select = arr.AsParallel()
                 .Select(Increment) // арифметическая функция для каждого элемента
                 .GroupBy(x => x % 10) // группировка по кратности элемента десяти
                 .Select(x => x.Sum()) // сумма групп
                 .OrderBy(x => x) // сортировка
-                .ForAll(x =>
+                .ToArray();
+                /*.ForAll(x =>
                 {
                     waiter.WaitOne();
                     if (n >= 5 && n <= 7) Console.Write($"{x} "); // вывод
                     n++;
                     waiter.Set();
-                });
+                });*/
+for (int i = 4; i < 7; i++) Console.Write($"{select[i]} "); // вывод
 time.Stop();
 Console.WriteLine($"\n\tвремя: {time.ElapsedTicks} ticks\n");
 
@@ -37,19 +39,22 @@ for (int i = 1; i < 17; i += 3)
 {
     n = 0;
     time.Restart();
-    arr.AsParallel()
+    var select0 = arr.AsParallel()
                 .WithDegreeOfParallelism(i) // использует не более одного процессора, если i == 1
                 .Select(Increment)
                 .GroupBy(x => x % 10)
                 .Select(x => x.Sum())
-                .OrderBy(x => x)
-                .ForAll(x =>
+                .OrderBy(x => x) // сортировка
+                .ToArray();
+                /*.ForAll(x =>
                 {
                     waiter.WaitOne();
-                    if (n >= 5 && n <= 7) Console.Write($"{x} "); // вывод
+                    if (n >= 5 && n <= 7) 
+                        Console.Write($"{x} "); // вывод
                     n++;
                     waiter.Set();
-                });
+                });*/
+    for (int j = 4; j < 7; j++) Console.Write($"{select0[j]} "); // вывод
     time.Stop();
     Console.WriteLine($"\n\tвремя (степень паралелизма {i}) {time.ElapsedTicks} ticks\n");
 }
